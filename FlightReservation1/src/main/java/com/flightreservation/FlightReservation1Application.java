cd3a1b1f96796db63dbb576e48c7576d5c4e2e0f
bhaveshchetwani <bhaveshchetwani@gmail.com>
2019-05-04 02:47:32 +0530

    Deployment changes -- Test

--- FlightReservation1/src/main/java/com/flightreservation/FlightReservation1Application.java
+++ FlightReservation1/src/main/java/com/flightreservation/FlightReservation1Application.java
@@ -2,11 +2,19 @@ package com.flightreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightReservation1Application {
+public class FlightReservation1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReservation1Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(FlightReservation1Application.class);
 	}
 }
